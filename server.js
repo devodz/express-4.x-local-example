8936e48784fac2e7003c840fa62b0d9b430f0a2a
devodz <51398414+devodz@users.noreply.github.com>
2019-06-12 15:33:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
-var db = require('./db');
+var db = require('./db');;
 
 
 // Configure the local strategy for use by Passport.
